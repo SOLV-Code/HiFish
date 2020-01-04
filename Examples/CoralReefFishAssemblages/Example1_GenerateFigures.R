079d3a5884a72f5854ea4cfcd9538507604466c4
SOLV-Code <gottfries.pestal@solv.ca>
2020-01-04 10:41:35 -0800

    fixed tornadoplot

--- Examples/CoralReefFishAssemblages/Example1_GenerateFigures.R
+++ Examples/CoralReefFishAssemblages/Example1_GenerateFigures.R
@@ -28,8 +28,8 @@ if(!is.null(labels)){text(0,y.ticks,labels=labels,adj=1.1,xpd=NA,col="darkblue")
 custom.barplot(data.df$IRD,labels=data.df$Common.name,xlab="IRD")
 title(main="Dominant Species")
 #custom.barplot(data.df$IRD,labels=NULL,xlab="IRD")
-
-
+#custom.barplot(data.df$Frequency,labels=data.df$Common.name,xlab="Frequency")
+#custom.barplot(data.df$Biomass,labels=data.df$Common.name,xlab="% of Biomass")
 
 custom.tornadoplot <- function(values,labels,xlab="Value"){
   # values = data frame with 2 numeric values (plotted on same axis)
@@ -44,12 +44,24 @@ custom.tornadoplot <- function(values,labels,xlab="Value"){
 
   print(par("usr"))
   rect(0,y.ticks-0.5,values[,2],y.ticks+0.5,col="lightblue",border="darkblue")
-  rect(0,y.ticks-0.5,-values[,1],y.ticks+0.5,col="lightblue",border="darkblue")
-  #if(!is.null(labels)){text(0,y.ticks,labels=labels,adj=1.1,xpd=NA,col="darkblue")}
+  rect(0,y.ticks-0.5,-values[,1],y.ticks+0.5,col="white",border="darkblue")
+  if(!is.null(labels)){text(par("usr")[1],y.ticks,labels=labels,adj=1.1,xpd=NA,col="darkblue")}
   
 }
 
+# test the tornadoplot
 custom.tornadoplot(data.df[,c("Frequency","Biomass")],labels=data.df$Common.name,xlab="Percent")
 title(main="Dominant Species")
+
+
+
+
+
+
+
+
+
+
+
 
 
